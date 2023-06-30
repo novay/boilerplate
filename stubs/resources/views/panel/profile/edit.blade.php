864f0cffe2ddab1cdaf39d24ff0f18997e6d3cb7
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2023-07-01 00:31:38 +0800

    Init

--- stubs/resources/views/panel/profile/edit.blade.php
+++ stubs/resources/views/panel/profile/edit.blade.php
@@ -1,29 +1,27 @@
 @seoTitle(__('Profile'))
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+        <x-breadcrumb>
+            <x-breadcrumb-item>{{ __('Profile') }}</x-breadcrumb-item>
+        </x-breadcrumb>
     </x-slot>
 
-    <div class="py-10 bg-gray-50">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-5">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl" dusk="update-profile-information">
-                    @include('panel.profile.partials.update-profile-information-form')
-                </div>
+    <div class="w-full p-4 lg:pl-60 space-y-4">
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl" dusk="update-profile-information">
+                @include('panel.profile.partials.update-profile-information-form')
             </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl" dusk="update-password">
-                    @include('panel.profile.partials.update-password-form')
-                </div>
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl" dusk="update-password">
+                @include('panel.profile.partials.update-password-form')
             </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl" dusk="delete-user">
-                    @include('panel.profile.partials.delete-user-form')
-                </div>
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl" dusk="delete-user">
+                @include('panel.profile.partials.delete-user-form')
             </div>
         </div>
     </div>
